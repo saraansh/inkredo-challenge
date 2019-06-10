e0f1e4fab838c61f91f96380e0b93bbabdf4ef7f
saraansh <bsaraansh@gmail.com>
2019-06-10 23:50:24 +0530

    Added option to register | login | logout user

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from .forms import UserRegistrationForm
 from .models import Company
 
 def home(request):
@@ -9,5 +12,18 @@ def home(request):
 	}
 	return render(request, 'dashboard/home.html', context)
 
+def register(request):
+	if request.method == 'POST':
+		form = UserRegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f'Account created for {username}!')
+			return redirect('dashboard-home')
+	else:
+		form = UserRegistrationForm()
+	return render(request, 'dashboard/register.html', {'form': form})
+
+@login_required
 def settings(request):
 	return render(request, 'dashboard/settings.html', {'title': 'Settings'})
